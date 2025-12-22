133c5a82d9d7f45bcd554b92238492b935edcc93
ksn-sumu <1207628648@qq.com>
2025-12-22 16:06:52 +0800

    初版，除了和上位机通信的内容

---
+++ Core/Inc/mos.h
@@ -0,0 +1,41 @@
+//
+// Created by 12076 on 2025/12/20.
+//
+
+#ifndef HUANQIDONG_MOS_H
+#define HUANQIDONG_MOS_H
+
+#include "main.h"
+
+#define MOS_CLOSE GPIO_PIN_RESET
+#define MOS_OPEN GPIO_PIN_SET
+
+#define is_in(num, threshold) (num >= threshold[0] && num <= threshold[1])
+
+typedef struct {
+	float vin_threshold[2];
+	float i1_threshold[2];
+	float i2_threshold[2];
+	float i3_threshold[2];
+	float i4_threshold[2];
+} config_t;
+
+typedef struct {
+	float vin;
+	float i1;
+	float i2;
+	float i3;
+	float i4;
+	GPIO_PinState mos_state[5];
+} state_t;
+
+extern config_t config;
+extern state_t state;
+
+void ctrl_one_mos(uint8_t mos, GPIO_PinState mos_state);
+void ctrl_all_mos(GPIO_PinState mos_state);
+void check_all(void);
+uint8_t config_load(void);
+uint8_t config_save(void);
+
+#endif // HUANQIDONG_MOS_H
